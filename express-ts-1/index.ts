b6bf2094abd2ef1ef182c5d4c3cf3d3419b2c789
gautam <gautamcvernekar@gmail.com>
2025-05-12 08:59:31 -0400

    u

--- express-ts-1/index.ts
+++ express-ts-1/index.ts
@@ -4,7 +4,7 @@ const app: Express = express()
 app.get('/', (req: Request, res: Response) => {
     res.send('Server 1')
 })
-///
+////
 
 app.listen(8000, () => {
     console.log('Server 1 Listening!!!')
